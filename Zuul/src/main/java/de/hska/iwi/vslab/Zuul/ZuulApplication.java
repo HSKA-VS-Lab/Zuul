9f29dbd651844cd79d77dc59ddf29bb9bc4765fc
BobFlop <BobFlop@users.noreply.github.com>
2020-01-06 23:16:45 +0100

    several experimentations, necessary commit to work on other workstation

--- Zuul/src/main/java/de/hska/iwi/vslab/Zuul/ZuulApplication.java
+++ Zuul/src/main/java/de/hska/iwi/vslab/Zuul/ZuulApplication.java
@@ -3,11 +3,13 @@ package de.hska.iwi.vslab.Zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+//@EnableDiscoveryClient // uses implementation of spring for discovery clients (like eureka)
+@EnableEurekaClient // uses implementation of eureka
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
 public class ZuulApplication {
 
 	// start the server, the eureka server and the core product service 
